bf857992ec46a5feed0b472a2c57f3f1c86a7cf9
Nicolas Josino <nicolasjosino@gmail.com>
2022-10-31 17:51:30 -0300

    Enemy addition

--- src/scenes/level-1/index.ts
+++ src/scenes/level-1/index.ts
@@ -2,6 +2,7 @@ import { GameObjects, Scene, Tilemaps } from "phaser";
 import { Player } from "../../classes/player";
 import { gameObjectsToObjectPoints } from "../../helpers/gameobject-to-object-point";
 import { EVENTS_NAME } from "../../consts";
+import { Enemy } from "../../classes/enemy";
 
 export class Level1 extends Scene {
   private player!: Player;
@@ -10,6 +11,7 @@ export class Level1 extends Scene {
   private wallsLayer!: Tilemaps.TilemapLayer;
   private groundLayer!: Tilemaps.TilemapLayer;
   private chests!: Phaser.GameObjects.Sprite[];
+  private enemies!: Phaser.GameObjects.Sprite[];
 
   constructor() {
     super("level-1-scene");
@@ -17,8 +19,9 @@ export class Level1 extends Scene {
 
   create(): void {
     this.initMap();
-    this.player = new Player(this, 100, 100);
+    this.player = new Player(this, 25, 100);
     this.initChests();
+    this.initEnemies();
     this.physics.add.collider(this.player, this.wallsLayer);
   }
 
@@ -27,10 +30,10 @@ export class Level1 extends Scene {
   }
 
   private initMap(): void {
-    this.map = this.make.tilemap(({ key: 'dungeon', tileWidth: 16, tileHeight: 16 }));
+    this.map = this.make.tilemap(({ key: "dungeon", tileWidth: 16, tileHeight: 16 }));
     this.tileset = this.map.addTilesetImage("dungeon", "tiles");
-    this.groundLayer = this.map.createLayer('Ground', this.tileset, 0, 0);
-    this.wallsLayer = this.map.createLayer('Walls', this.tileset, 0, 0);
+    this.groundLayer = this.map.createLayer("Ground", this.tileset, 0, 0);
+    this.wallsLayer = this.map.createLayer("Walls", this.tileset, 0, 0);
     this.wallsLayer.setCollisionByProperty({ collides: true });
     this.physics.world.setBounds(0, 0, this.wallsLayer.width, this.wallsLayer.height);
     // this.showDebugWalls();
@@ -40,8 +43,10 @@ export class Level1 extends Scene {
     const chestPoints = gameObjectsToObjectPoints(
       this.map.filterObjects("Chests", (obj) => obj.name === "ChestPoint")
     );
-    this.chests = chestPoints.map(chestPoint =>
-      this.physics.add.sprite(chestPoint.x, chestPoint.y, 'tiles_spr', 595).setScale(1.5),
+    this.chests = chestPoints.map((chestPoint) =>
+      this.physics.add
+        .sprite(chestPoint.x, chestPoint.y, "tiles_spr", 595)
+        .setScale(1.5)
     );
     this.chests.forEach((chest) => {
       this.physics.add.overlap(this.player, chest, (obj1, obj2) => {
@@ -49,7 +54,23 @@ export class Level1 extends Scene {
         obj2.destroy();
       });
     });
-  }  
+  }
+
+  private initEnemies(): void {
+    const enemiesPoints = gameObjectsToObjectPoints(
+      this.map.filterObjects("Enemies", (obj) => obj.name === "EnemyPoint")
+    );
+    this.enemies = enemiesPoints.map((enemyPoint) =>
+      new Enemy(this, enemyPoint.x, enemyPoint.y, "tiles_spr", this.player, 503)
+        .setName(enemyPoint.id.toString())
+        .setScale(1.5)
+    );
+    this.physics.add.collider(this.enemies, this.wallsLayer);
+    this.physics.add.collider(this.enemies, this.enemies);
+    this.physics.add.collider(this.player, this.enemies, (obj1, obj2) => {
+      (obj1 as Player).getDamage(1);
+    });
+  }
 
   private showDebugWalls(): void {
     const debugGraphics = this.add.graphics().setAlpha(0.7);
--- src/scenes/loading/index.ts
+++ src/scenes/loading/index.ts
@@ -1,27 +1,32 @@
-import { GameObjects, Scene } from 'phaser';
+import { GameObjects, Scene } from "phaser";
 export class LoadingScene extends Scene {
-  private king!: GameObjects.Sprite;
+  private dungeonMap!: string;
   constructor() {
-    super('loading-scene');
+    super("loading-scene");
   }
   create(): void {
-    this.scene.start('level-1-scene');
-    this.scene.start('ui-scene');
+    this.scene.start("level-1-scene");
+    this.scene.start("ui-scene");
   }
 
   preload(): void {
-    this.load.baseURL = 'assets/';
+    this.load.baseURL = "assets/";
 
-    this.load.spritesheet('tiles_spr', 'tilemaps/tiles/dungeon-16-16.png', {
+    this.load.spritesheet("tiles_spr", "tilemaps/tiles/dungeon-16-16.png", {
       frameWidth: 16,
       frameHeight: 16,
-    });    
-    
-    this.load.image('tiles', 'tilemaps/tiles/dungeon-16-16.png');
-    this.load.tilemapTiledJSON('dungeon', 'tilemaps/json/dungeons-01.tmj');    
+    });
 
-    this.load.image('king', 'sprites/king.png');
-    this.load.image('king', 'sprites/king.png');
-    this.load.atlas('a-king', 'spritesheets/a-king.png', 'spritesheets/a-king_atlas.json');
+    this.dungeonMap = "dungeons-01";
+
+    this.load.image("tiles", "tilemaps/tiles/dungeon-16-16.png");
+    this.load.tilemapTiledJSON(
+      "dungeon",
+      `tilemaps/json/${this.dungeonMap}.tmj`
+    );
+
+    this.load.image("king", "sprites/king.png");
+    this.load.image("king", "sprites/king.png");
+    this.load.atlas("a-king", "spritesheets/a-king.png", "spritesheets/a-king_atlas.json");
   }
 }
