5f2edc01afbdbf272a2e1d20d0f0df8851ee2183
Nicolas Josino <nicolasjosino@gmail.com>
2022-11-28 17:06:21 -0300

    More random levels added

--- src/classes/levelData.ts
+++ src/classes/levelData.ts
@@ -4,7 +4,7 @@ export class LevelData extends Object {
     
     constructor() {
         super();
-        this.levels = ["01", "02", "03", "04", "05"]//, "06", "07", "08", "09", "10"];
+        this.levels = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10"];
         this.count = 0;
     }
 }
